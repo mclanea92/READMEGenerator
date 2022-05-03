d6ddfeb038683fe3087b6ca2439a9288057e55e3
Adam McLane <mclanea92@gmail.com>
2022-05-03 13:44:31 -0400

    added questions and found licenses to use

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# READMEGenerator
+# Title
+
+## Description
+
+
+## Table of Contents
+
+
+## Installation
+
+
+## Usage
+
+
+## License
+
+## Contributing
+
+## Tests
+
+
+## Questions
+
+licenses
+Apache 2.0
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+https://opensource.org/licenses/Apache-2.0
+
+The MIT
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+https://opensource.org/licenses/MIT
+
+Monzilla Public License 2.0
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+https://opensource.org/licenses/MPL-2.0
--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const makeReadMe = require('./utils/generateMarkdown');
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Name of Project.'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description of your project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the intended use for this application?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are you contributing?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you run a test?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license are you using?',
+        choices: ['Apache 2.0', 'The MIT', 'Monzilla Public License 2.0']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'contact',
+        message: 'What is your email address?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
 init();
