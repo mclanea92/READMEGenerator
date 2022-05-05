5da09ff0ee02e80e1cd48d6c324f4a16575e14b5
Adam McLane <mclanea92@gmail.com>
2022-05-05 15:15:14 -0400

    changed file name

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ return inquirer.prompt([
 .then((answers) => {
     console.log(answers)
     const getContent = generateMarkdown(answers);
-    fs.writeFile('projectReadMe.md', getContent, (err) =>
+    fs.writeFile('ProjectReadMe.md', getContent, (err) =>
       err ? console.error(err) : console.log('Success!'));
 }
 )}
